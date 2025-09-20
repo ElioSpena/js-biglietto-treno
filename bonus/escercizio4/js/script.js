20db6ede3a019d3ee397f2bead23aa7c28b5ca24
ElioSpena <elio.spena.94@gmail.com>
2025-09-20 12:06:44 +0200

    feat: esercizio 4

---
+++ bonus/escercizio4/js/script.js
@@ -0,0 +1,6 @@
+const userName = prompt("Qual è il tuo nome?", "Elio");
+const userSurname = prompt("Qual è il tuo cognome?", "Spena");
+const userColor = prompt("Qual è il tuo colore preferito?", "Blu");
+let message = `${userName}${userSurname}${userColor}`;
+
+console.log(message);
